82f73fbf7651fd2c14275ad1726fbe83956fd761
Sofiia Hoppova <sofiahoppova@gmail.com>
2024-05-13 11:26:56 +0300

    add tasks

---
+++ js/task-2.js
@@ -0,0 +1,34 @@
+class Storage{
+  #items = [];
+
+  constructor(elements){
+    this.#items = elements;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    if (this.#items.includes(itemToRemove)) {
+      const index = this.#items.indexOf(itemToRemove);
+      this.#items.splice(index, 1);
+    }
+  }
+}
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
